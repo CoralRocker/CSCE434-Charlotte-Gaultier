6253cf13fb48ca359a70234f6df64e74e43f400a
Gaultier Delbarre <gaultiertexas@gmail.com>
2023-09-23 14:12:20 -0500

    Add type to Symbol

--- src/coco/Symbol.java
+++ src/coco/Symbol.java
@@ -1,17 +1,25 @@
 package coco;
 
+import java.util.ArrayList;
+
 public class Symbol {
 
     private String name;
 
+    private ArrayType type;
+
     // TODO: Add other parameters like type
 
-    public Symbol (String name) {
+    public Symbol (String name, ArrayType type) {
         this.name = name;
+        this.type = type;
     }
     public String name () {
         return name;
     }
 
+    public ArrayType type() {
+        return type;
+    }
 
 }
