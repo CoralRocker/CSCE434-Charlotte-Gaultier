3ded9d4e1cae268409778bdca8245633ad690baa
Gaultier Delbarre <gaultiertexas@gmail.com>
2023-09-29 15:39:11 -0500

    Added easy-to-implement base for visitors

--- src/ast/UnresolvedFunctionVisitor.java
+++ src/ast/UnresolvedFunctionVisitor.java
@@ -4,9 +4,7 @@ import coco.Token;
 
 import java.util.ArrayList;
 
-public class UnresolvedFunctionVisitor implements NodeVisitor {
-
-    private int depth = 0;
+public class UnresolvedFunctionVisitor extends VisitorBase {
 
     private ArrayList<Token> unresolvedTokens;
 
@@ -18,230 +16,26 @@ public class UnresolvedFunctionVisitor implements NodeVisitor {
         unresolvedTokens = new ArrayList<Token>();
     }
 
-    private String getTabs() {
-        StringBuilder tabs = new StringBuilder();
-        for( int i = 0; i < depth; i++ )
-            tabs.append("  ");
-        return tabs.toString();
-    }
-
     @Override
-    public void visit(Addition add) {
-        depth++;
-        add.getLvalue().accept(this);
-        add.getRvalue().accept(this);
-        depth--;
-    }
+    public void exprAction(AST expr) {
+        if( expr instanceof FuncCall ) {
+            FuncCall call = (FuncCall) expr;
+            if( !call.func.hasType() ) {
+                unresolvedTokens.add(call.funcTok);
+            }
 
-    @Override
-    public void visit(ArgList list) {
-        depth++;
-        for( AST ast : list.getArgs() ) {
-            ast.accept(this);
-        }
-        depth--;
-    }
-
-    @Override
-    public void visit(ArrayIndex idx) {
-        depth++;
-        idx.getArray().accept(this);
-        idx.getIndex().accept(this);
-        depth--;
-    }
-
-    @Override
-    public void visit(Assignment asn) {
-        depth++;
-        asn.getTarget().accept(this);
-        asn.getRvalue().accept(this);
-        depth--;
-    }
-
-    @Override
-    public void visit(BoolLiteral bool) {}
-
-    @Override
-    public void visit(DeclarationList list) {
-        depth++;
-        for( AST decl : list.getContained() ) {
-            decl.accept(this);
-        }
-        depth--;
-
-    }
-
-    @Override
-    public void visit(Designator des) {
-    }
-
-    @Override
-    public void visit(Division div) {
-        depth++;
-        div.getLvalue().accept(this);
-        div.getRvalue().accept(this);
-        depth--;
-    }
-
-    @Override
-    public void visit(FloatLiteral flt) {
-    }
-
-    @Override
-    public void visit(FuncBody fb) {
-        depth++;
-        if( fb.getVarList() != null )
-            fb.getVarList().accept(this);
-        fb.getSeq().accept(this);
-        depth--;
-    }
-
-    @Override
-    public void visit(FuncCall fc) {
-        if( !fc.func.hasType() ) {
-            unresolvedTokens.add(fc.funcTok);
         }
 
-        depth++;
-        fc.getArgs().accept(this);
-        depth--;
     }
 
     @Override
-    public void visit(FuncDecl fd) {
-        depth++;
-        fd.getBody().accept(this);
-        depth--;
-    }
-
-    @Override
-    public void visit(IfStat is) {
-        depth++;
-        is.getIfrel().accept(this);
-        is.getIfseq().accept(this);
-        if( is.getElseseq() != null ) {
-            is.getElseseq().accept(this);
-        }
-        depth--;
-    }
-
-    @Override
-    public void visit(IntegerLiteral il) {
-    }
-
-    @Override
-    public void visit(LogicalAnd la) {
-        depth++;
-        la.getLvalue().accept(this);
-        la.getRvalue().accept(this);
-        depth--;
-    }
-
-    @Override
-    public void visit(LogicalNot ln) {
-        depth++;
-        ln.getRvalue().accept(this);
-        depth--;
-    }
-
-    @Override
-    public void visit(LogicalOr lo) {
-        depth++;
-        lo.getLvalue().accept(this);
-        lo.getRvalue().accept(this);
-        depth--;
-    }
-
-    @Override
-    public void visit(Modulo mod) {
-        depth++;
-        mod.getLvalue().accept(this);
-        mod.getRvalue().accept(this);
-        depth--;
-    }
-
-    @Override
-    public void visit(Multiplication mul) {
-        depth++;
-        mul.getLvalue().accept(this);
-        mul.getRvalue().accept(this);
-        depth--;
-    }
-
-    @Override
-    public void visit(Power pwr) {
-        depth++;
-        pwr.getLvalue().accept(this);
-        pwr.getRvalue().accept(this);
-        depth--;
+    public void statAction(AST stat) {
 
     }
 
     @Override
-    public void visit(Relation rel) {
-        depth++;
-        rel.getLvalue().accept(this);
-        rel.getRvalue().accept(this);
-        depth--;
+    public void compAction(RootAST comp) {
 
     }
 
-
-    @Override
-    public void visit(RepeatStat rep) {
-        depth++;
-        rep.getRelation().accept(this);
-        rep.getSeq().accept(this);
-        depth--;
-    }
-
-    @Override
-    public void visit(Return ret) {
-        if( ret.getReturn() != null ) {
-            depth++;
-            ret.getReturn().accept(this);
-            depth--;
-        }
-    }
-
-    @Override
-    public void visit(RootAST root) {
-        depth++;
-        if( root.getVars() != null )
-            root.getVars().accept(this);
-        if( root.getFuncs() != null )
-            root.getFuncs().accept(this);
-        root.getSeq().accept(this);
-        depth--;
-    }
-
-    @Override
-    public void visit(StatSeq seq) {
-        depth++;
-        for( AST ast : seq.getSequence() ) {
-            ast.accept(this);
-        }
-        depth--;
-    }
-
-    @Override
-    public void visit(Subtraction sub) {
-        depth++;
-        sub.getLvalue().accept(this);
-        sub.getRvalue().accept(this);
-        depth--;
-
-    }
-
-    @Override
-    public void visit(VariableDeclaration var) {
-    }
-
-    @Override
-    public void visit(WhileStat wstat) {
-        depth++;
-        wstat.getRelation().accept(this);
-        wstat.getSeq().accept(this);
-        depth--;
-    }
 }
---
+++ src/ast/VisitorBase.java
@@ -0,0 +1,239 @@
+package ast;
+
+import coco.Token;
+
+import java.util.ArrayList;
+
+public abstract class VisitorBase implements NodeVisitor {
+
+    public VisitorBase() {
+    }
+
+    public abstract void exprAction(AST expr);
+    public abstract void statAction(AST stat);
+
+    public abstract void compAction(RootAST comp);
+
+    @Override
+    public void visit(Addition add) {
+        exprAction(add);
+        add.getLvalue().accept(this);
+        add.getRvalue().accept(this);
+        
+    }
+
+    @Override
+    public void visit(ArgList list) {
+        exprAction(list);
+        for( AST ast : list.getArgs() ) {
+            ast.accept(this);
+        }
+        
+    }
+
+    @Override
+    public void visit(ArrayIndex idx) {
+        exprAction(idx);
+        idx.getArray().accept(this);
+        idx.getIndex().accept(this);
+        
+    }
+
+    @Override
+    public void visit(Assignment asn) {
+        statAction(asn);
+        asn.getTarget().accept(this);
+        asn.getRvalue().accept(this);
+        
+    }
+
+    @Override
+    public void visit(BoolLiteral bool) {
+        exprAction(bool);
+    }
+
+    @Override
+    public void visit(DeclarationList list) {
+        statAction(list);
+        for( AST decl : list.getContained() ) {
+            decl.accept(this);
+        }
+        
+
+    }
+
+    @Override
+    public void visit(Designator des) {
+        exprAction(des);
+    }
+
+    @Override
+    public void visit(Division div) {
+        exprAction(div);
+        div.getLvalue().accept(this);
+        div.getRvalue().accept(this);
+        
+    }
+
+    @Override
+    public void visit(FloatLiteral flt) {
+        exprAction(flt);
+    }
+
+    @Override
+    public void visit(FuncBody fb) {
+        statAction(fb);
+        if( fb.getVarList() != null )
+            fb.getVarList().accept(this);
+        fb.getSeq().accept(this);
+        
+    }
+
+    @Override
+    public void visit(FuncCall fc) {
+        exprAction(fc);
+        fc.getArgs().accept(this);
+        
+    }
+
+    @Override
+    public void visit(FuncDecl fd) {
+        statAction(fd);
+        fd.getBody().accept(this);
+        
+    }
+
+    @Override
+    public void visit(IfStat is) {
+        statAction(is);
+        is.getIfrel().accept(this);
+        is.getIfseq().accept(this);
+        if( is.getElseseq() != null ) {
+            is.getElseseq().accept(this);
+        }
+        
+    }
+
+    @Override
+    public void visit(IntegerLiteral il) {
+        exprAction(il);
+    }
+
+    @Override
+    public void visit(LogicalAnd la) {
+        exprAction(la);
+        la.getLvalue().accept(this);
+        la.getRvalue().accept(this);
+        
+    }
+
+    @Override
+    public void visit(LogicalNot ln) {
+        exprAction(ln);
+        ln.getRvalue().accept(this);
+        
+    }
+
+    @Override
+    public void visit(LogicalOr lo) {
+        exprAction(lo);
+        lo.getLvalue().accept(this);
+        lo.getRvalue().accept(this);
+        
+    }
+
+    @Override
+    public void visit(Modulo mod) {
+        exprAction(mod);
+        mod.getLvalue().accept(this);
+        mod.getRvalue().accept(this);
+        
+    }
+
+    @Override
+    public void visit(Multiplication mul) {
+        exprAction(mul);
+        mul.getLvalue().accept(this);
+        mul.getRvalue().accept(this);
+        
+    }
+
+    @Override
+    public void visit(Power pwr) {
+        exprAction(pwr);
+        pwr.getLvalue().accept(this);
+        pwr.getRvalue().accept(this);
+        
+
+    }
+
+    @Override
+    public void visit(Relation rel) {
+        exprAction(rel);
+        rel.getLvalue().accept(this);
+        rel.getRvalue().accept(this);
+        
+
+    }
+
+
+    @Override
+    public void visit(RepeatStat rep) {
+        statAction(rep);
+        rep.getRelation().accept(this);
+        rep.getSeq().accept(this);
+        
+    }
+
+    @Override
+    public void visit(Return ret) {
+        statAction(ret);
+        if( ret.getReturn() != null ) {
+            
+            ret.getReturn().accept(this);
+            
+        }
+    }
+
+    @Override
+    public void visit(RootAST root) {
+        compAction(root);
+        if( root.getVars() != null )
+            root.getVars().accept(this);
+        if( root.getFuncs() != null )
+            root.getFuncs().accept(this);
+        root.getSeq().accept(this);
+        
+    }
+
+    @Override
+    public void visit(StatSeq seq) {
+        statAction(seq);
+        for( AST ast : seq.getSequence() ) {
+            ast.accept(this);
+        }
+        
+    }
+
+    @Override
+    public void visit(Subtraction sub) {
+        exprAction(sub);
+        sub.getLvalue().accept(this);
+        sub.getRvalue().accept(this);
+        
+
+    }
+
+    @Override
+    public void visit(VariableDeclaration var) {
+        statAction(var);
+    }
+
+    @Override
+    public void visit(WhileStat wstat) {
+        statAction(wstat);
+        wstat.getRelation().accept(this);
+        wstat.getSeq().accept(this);
+        
+    }
+}
